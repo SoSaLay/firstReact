92bd1b20b6e51f1729228565116a526e3ece82ad
Sulay <Sulaymank704@gmail.com>
2024-07-09 15:56:44 -0400

    final

--- App.js
+++ App.js
@@ -1,26 +1,90 @@
 import React, { Component } from 'react';
-import info from './info.js'
+//  imports React library and the Component class 
+//necessary for building UI components
+
+import Info from './info';
+//imports Info component from the info.js 
 
 class App extends Component {
+  //props is an argument which contains any passed-in properties.
 constructor(props){
   super(props)
+  
+  //an object with a single property
   this.state={
-    person:{
+    personList:[
+      
+      {
       name:'Sulayman K',
       dob:'April 15, 5000',
       phone:'704-704-7044'
+    },
+    {
+      name:'SulK',
+      dob:'March 15, 2000',
+      phone:'704-704-7044'
+    },
+    {
+      name:'Ronr',
+      dob:'April 15, 2300',
+      phone:'704-704-7044'
+    },
+    {
+      name:'Lenny',
+      dob:'April 15, 2300',
+      phone:'704-704-7044'
+    },
+    {
+      name:'Wick',
+      dob:'April 15, 2300',
+      phone:'704-704-7044'
     }
+  
+  ]
+
+  
   }
 }
 
-
+//defines what the UI should look like.
   render() {
+    //specifies the JSX to be rendered to the DOM
     return (
       <div>
-      <info person ={this.state.person}/>
+        {this.state.personList.map((person,index)=>(
+          <Info key ={index} person={person}/>
+        ))}
       </div>
     );
   }
 }
 
+
 export default App;
+
+
+//Very Easy
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>My Basic Information</h1>
+//         <p>Name: Sulayman K</p>
+//         <p>Phone: 704-704-7044</p>
+//         <p>Date of Birth: April 18, 500</p>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
+
+
+
+
+
+
+
--- info.js
+++ info.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 
-class info extends Component{
+class Info extends Component{
 render(){
 
 const{name,dob,phone}= this.props.person;
+//accesses the properties from person within app.js
 
+
+//specifies the JSX to be rendered to the DOM when componet is called
 return(
 
-    <div>
+    <div style={{marginBottom:'20px'}}>
     <h1>My Basic Information</h1>
     <p>Name: {name}</p>
     <p>Phone: {phone}</p>
@@ -16,9 +19,7 @@ return(
 
 )
 
-
-
 }
 }
 
-export default info;
+export default Info;
